b97ef8eae5c91c5b40d862f8fefd505a6cdde0d0
fadi alhabib <fadi.b.alhabib@gmail.com>
2025-09-24 01:32:31 +0300

    agents

--- src/agents/agents.module.ts
+++ src/agents/agents.module.ts
@@ -1,4 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AgentsEntity } from "./entities/agents.entity";
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([AgentsEntity])],
+})
 export class AgentsModule {}
