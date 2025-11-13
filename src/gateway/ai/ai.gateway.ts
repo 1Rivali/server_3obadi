e9566be0b4ab6bc6f8d3f91da4c1fedababf7cc5
fadi alhabib <fadi.b.alhabib@gmail.com>
2025-11-14 00:03:29 +0200

    api

--- src/gateway/ai/ai.gateway.ts
+++ src/gateway/ai/ai.gateway.ts
@@ -1,17 +1,17 @@
 import { post } from "src/lib/apiClient";
 import { IsMetalizedResponse } from "./metalized.response";
 import { MetalizedRequest } from "./meatalized.request";
-import FormData = require("form-data");
+
 import https from "https";
 
 export const isMetalised = async (
   file: Express.Multer.File
 ): Promise<IsMetalizedResponse> => {
   const form = new FormData();
-  form.append("file", file.buffer, {
-    filename: file.originalname,
-    contentType: file.mimetype,
-  });
+  const blob = new Blob([file.buffer], { type: file.mimetype });
+
+  // Append the file with original filename
+  form.append("file", blob, file.originalname);
 
   const allowInsecure = process.env.AI_ALLOW_INSECURE === "true";
   const httpsAgent = allowInsecure
