1f9fbaa0e0f737563b83a7f8e97f09591d8809b8
fadi alhabib <fadi.b.alhabib@gmail.com>
2025-11-20 00:31:04 +0200

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { join } from 'path';
-import * as serveStatic from 'serve-static';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { join } from "path";
+import * as serveStatic from "serve-static";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(serveStatic(join(__dirname, '..', 'web')));
+
+  // Enable CORS for all origins
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "Accept"],
+  });
+
+  app.use(serveStatic(join(__dirname, "..", "web")));
   await app.listen(3000);
 }
 bootstrap();
