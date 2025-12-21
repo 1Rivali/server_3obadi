18d4abea601e57ef64b8bd2d183194831b9146b0
fadi alhabib <fadi.b.alhabib@gmail.com>
2025-12-21 23:28:33 +0200

    logs

--- src/transitions/services/mtn.service.ts
+++ src/transitions/services/mtn.service.ts
@@ -226,9 +226,9 @@ export class MtnService {
     }
 
     this.logger.warn(
-      `Unexpected response from MTN API - Result: ${response.data}, Error: ${
-        response.data.Error || "N/A"
-      }, mobile: ${mobile}`
+      `Unexpected response from MTN API - Result: ${JSON.parse(
+        response.data
+      )}, Error: ${response.data.Error || "N/A"}, mobile: ${mobile}`
     );
   }
 
